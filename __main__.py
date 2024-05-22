915de546470f509dc94e7a5d478d987c54883e8e
VovLer G <126910533+DaregonPL@users.noreply.github.com>
2024-05-22 17:03:03 +0300

    update v1.1
    Added:
    - Table Controller:
      = method run(self)
      = method del_row(self, column, value)
      = renamed add_row -> sqlrow
      = methd add_row(self, **rowdata)
        more comfrtable:
            old: add_row(('val1', 'val2'), ('col1', 'col2'))
            new: add_row(col1='val1', col2='val2')

--- __main__.py
+++ __main__.py
@@ -1,7 +1,5 @@
-
-import copy
-
 import psycopg2
+from tabulate import tabulate
 
 
 class Database():
@@ -38,6 +36,7 @@ class Database():
         cur = self.con.cursor()
         cur.execute(command)
         cur.close()
+        self.con.commint()
 
     def get_tables(self):
         command = "SELECT table_schema, table_name FROM information_schema.tables WHERE table_type = 'BASE TABLE' AND table_schema NOT IN ('information_schema', 'pg_catalog') ORDER BY table_schema, table_name;"
@@ -56,13 +55,7 @@ class Table():
         self.name = name
 
     def __str__(self):
-        tb = [tuple(self.get_columns()), *self.get()]
-        res = [[str(y) for y in x] for x in tb]
-        ml = {}
-        for r in res:
-            for x in range(len(r)):
-                ml[x] = len(r[x]) if ml.get(x, 0) < len(r[x]) else ml[x]
-        return '\n'.join(['  '.join([r[x].ljust(ml[x]) for x in range(len(r))]) for r in res])
+        return tabulate(self.get(), headers=self.get_columns(), tablefmt='rounded_outline', numalign="left", stralign="left")
 
     def __iter__(self):
         self.iterrow = 0
@@ -80,50 +73,60 @@ class Table():
         self.db.con.commit()
         print('Table saved')
 
-    def get(self):
+    def drop(self):
+        self.run(f'Drop table {self.name}')
+        self.save()
+
+    def run(self, command):
         cur = self.db.con.cursor()
-        cur.execute(f'SELECT * FROM {self.name}')
-        output = cur.fetchall()
+        cur.execute(command)
+        try:
+            o = cur.fetchall()
+        except:
+            o = ''
         cur.close()
-        return output
+        return o
+
+    def get(self):
+        return self.run(f'SELECT * FROM {self.name}')
 
     def get_columns(self):
         command = f"SELECT column_name FROM information_schema.columns WHERE table_name = '{self.name.split('.')[1]}' AND table_schema = '{self.name.split('.')[0]}' ORDER BY ordinal_position"
-        cur = self.db.con.cursor()
-        cur.execute(command)
-        output = cur.fetchall()
-        cur.close()
-        return [x[0] for x in output]
+        return [x[0] for x in self.run(command)]
 
     def add_column(self, name: str, datatype: str, *args: str, **kw):
         deff = ' '.join([f'{arg} {value}' for arg, value in kw.items()]) + ' '.join(args)
         command = [f'ALTER TABLE {self.name}',
                    f'ADD {name} {datatype} {deff}']
-        cur = self.db.con.cursor()
-        cur.execute('\n'.join(command))
-        cur.close()
+        self.run('\n'.join(command))
         self.save()
 
     def edit_column(self, name: str, *args: str, **kw):
         deff = ' '.join([f'{arg} {value}' for arg, value in kw.items()]) + ' '.join(args)
         command = [f'ALTER TABLE {self.name}', f'ALTER COLUMN {name}', f'SET {deff}']
-        cur = self.db.con.cursor()
-        cur.execute('\n'.join(command))
-        cur.close()
+        self.run('\n'.join(command))
         self.save()
 
     def del_column(self, name: str):
-        cur = self.db.con.cursor()
-        cur.execute(f'ALTER TABLE {self.name} DROP COLUMN {name}')
-        cur.close()
+        self.run(f'ALTER TABLE {self.name} DROP COLUMN {name}')
         self.save()
 
-    def add_row(self, row_values: tuple, columns='all'):
+    def add_row(self, **rowdata):
+        cols = f"({', '.join([x for x in rowdata.keys()])})"
+        roww = ["'"+str(x)+"'" for x in rowdata.values()]
+        row_values = f"({', '.join(roww)})"
+        command = f'INSERT INTO {self.name} {cols} VALUES {row_values}'
+        self.run(command)
+        self.save()
+
+    def del_row(self, column, value):
+        self.run(f'DELETE FROM {self.name} WHERE {column} = {value}')
+        self.save()
+
+    def sqlrow(self, row_values: tuple, columns='all'):
         cols = f"({', '.join(columns)})" if type(columns) is tuple else ""
         command = f'INSERT INTO {self.name} {cols} VALUES {row_values}'
-        cur = self.db.con.cursor()
-        cur.execute(command)
-        cur.close()
+        self.run(command)
         self.save()
 
 
@@ -136,5 +139,6 @@ def pout(cursor: psycopg2.extensions.cursor):
 
 con = psycopg2.connect(database="8V", user="postgres", password="admin", host="localhost", port="5432")
 db = Database(con)
-for x in db:
-    print(x.name)
+clans = db.get_table('clans')
+players = db.get_table('players')
+print(db)
